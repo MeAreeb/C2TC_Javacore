8bc68bc35c638d9fa77a2f896349dc11d495638a
MeAreeb <100773184+MeAreeb@users.noreply.github.com>
2022-03-02 18:16:01 +0530

    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

---
+++ DAY2/src/addition.java
@@ -0,0 +1,12 @@
+
+public class addition {
+
+	public static void main(String[] args) {
+		
+		int a = 50;
+		int b = 30;
+		int c = a+b ;
+		System.out.println("The Sum of a and b is " +c);
+		
+	}
+}
