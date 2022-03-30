7eb1255544118af8bed654304c66c8d66850a582
MeAreeb <100773184+MeAreeb@users.noreply.github.com>
2022-03-30 19:04:22 +0530

    collection Day3

---
+++ Programs/src/collection/HashmapDemo.java
@@ -0,0 +1,12 @@
+package collection;
+
+import java.util.HashMap;
+public class HashmapDemo {
+
+	public static void main(String[] args) {
+		HashMap<Integer,String> Hmap=new HashMap<Integer,String>();
+ Hmap.put(101, "areeb");
+ System.out.println(Hmap);
+	}
+
+}
